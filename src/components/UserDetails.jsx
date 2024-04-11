977feb1fc595ccc3d54971350f23bde78babecf4
Anderes <andres10000fky@gmail.com>
2024-04-11 10:50:30 -0400

    React 1.5

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -3,6 +3,7 @@ import avatarJessica from '../assets/avatar-jessica.jpeg'
 import './UserDetails.css'
 
 const UserDetails = () => {
+    fetch('https://661038100640280f219c9a20.mockapi.io/api/v1/users').then(response => { })
     return (
         <div className="conteiner-details">
             <img src={avatarJessica} className="imagen" />
