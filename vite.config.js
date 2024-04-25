92b7f3ace5993bf76f334e5c2f2f1a20f2e663d5
Anderes <andres10000fky@gmail.com>
2024-04-24 22:45:26 -0400

    Rick And Morty 1.6 Navegacion

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://theanderes.github.io/React_Social_Links_Profile",
+  base: "https://theanderes.github.io/React_Social_Links_Profile/",
 })
